218d9d539588be8e42c1ce6f4b48955bc01f2f39
eNobreg <ezra.nobrega@outlook.com>
2019-12-10 14:44:59 -0500

    changed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 101):
     if i / 10 < i % 10:
         if i < 89:
-            print("{:d}".format(i), end=", ")
+            print("{:02d}".format(i), end=", ")
         else:
-            print("{:d}".format(i))
+            print("{:2d}".format(i))

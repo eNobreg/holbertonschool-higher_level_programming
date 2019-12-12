d1395e1204bc26ec4ac0baa47b461458f7d01b92
eNobreg <ezra.nobrega@outlook.com>
2019-12-12 14:34:51 -0500

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-   
+
     length = len(argv)
-    if length == 3:
+    if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif argv[2] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif argv[2] == '*': 
+    elif argv[2] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
     elif argv[2] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))

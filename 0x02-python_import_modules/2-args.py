4f3b99a227afe158e741d9f4ff9dec06a8b39503
eNobreg <ezra.nobrega@outlook.com>
2019-12-12 12:38:28 -0500

    firs tcommits

--- 0x02-python_import_modules/2-args.py
+++ 0x02-python_import_modules/2-args.py
@@ -2,6 +2,12 @@
 if __name__ == "__main__":
     import sys
 argc = len(sys.argv)
-print("{} arguments:".format(argc - 1))
-for i in range(1, argc):
-    print("{}: {}".format(i, sys.argv[i]))
+if argc == 1:
+    print("{} arguments.".format(argc - 1))
+elif argc == 2:
+    print("{} argument:".format(argc - 1))
+    print("{}: {}".format(1, sys.argv[1]))
+else:
+    print("{} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, sys.argv[i]))

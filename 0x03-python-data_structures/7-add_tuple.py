d578fb4160297a34262c68d6c0ced0ad02ea938e
eNobreg <ezra.nobrega@outlook.com>
2019-12-17 14:25:22 -0500

    test

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,4 @@ def add_tuple(a=(), b=()):
     new_1 = a + ("0", "0")
     new_2 = b + ("0", "0")
 
-    return [int(new_1[i]) + int(new_2[i]) for i in range(len(a))]
+    return [int(new_1[i]) + int(new_2[i]) for i in range(2)]

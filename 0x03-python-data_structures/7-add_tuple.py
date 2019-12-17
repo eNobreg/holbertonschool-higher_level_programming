97160503520c82d3bdbee46f59d1dddec71b63d9
eNobreg <ezra.nobrega@outlook.com>
2019-12-17 14:28:05 -0500

    update because list comprehension wasn't working

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,6 @@ def add_tuple(a=(), b=()):
     new_1 = a + ("0", "0")
     new_2 = b + ("0", "0")
 
-    return [int(new_1[i]) + int(new_2[i]) for i in range(2)]
+    sumA = int(new_1[0]) + int (new_2[0])
+    sumB = int(new_1[1]) + int (new_2[1])
+    return sumA, sumB

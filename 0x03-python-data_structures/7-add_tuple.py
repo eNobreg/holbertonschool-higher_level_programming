f2ed760bb3dbf627749e13f9056a76082b0c2f30
eNobreg <ezra.nobrega@outlook.com>
2019-12-17 14:29:00 -0500

    fixed whitespaces

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(a=(), b=()):
     new_1 = a + ("0", "0")
     new_2 = b + ("0", "0")
 
-    sumA = int(new_1[0]) + int (new_2[0])
-    sumB = int(new_1[1]) + int (new_2[1])
+    sumA = int(new_1[0]) + int(new_2[0])
+    sumB = int(new_1[1]) + int(new_2[1])
     return sumA, sumB

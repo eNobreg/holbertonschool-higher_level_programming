671e1732ca80b4b951b337f89cc9085cc972e714
eNobreg <ezra.nobrega@outlook.com>
2019-12-19 18:49:56 -0500

    [update] removed redundant statement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-
 
     if type(roman_string) is str:
         for i in range(len(roman_string)):
-            if i > 0 and rom_dict[roman_string[i]] > rom_dict[roman_string[i - 1]]:
-                num += rom_dict[roman_string[i]] - 2 * rom_dict[roman_string[i - 1]]
+            if i > 0 and rom[roman_string[i]] > rom[roman_string[i - 1]]:
+                num += rom[roman_string[i]] - 2
             else:
-                num += rom_dict[roman_string[i]]
+                num += rom[roman_string[i]]
         return num
-    return 0 
+    return 0

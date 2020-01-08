28b87a336e95a66189024aace0347891ea2e0ea4
eNobreg <ezra.nobrega@outlook.com>
2020-01-08 13:00:48 -0500

    [Change] I forget I left in the middle of typing this

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    c = None
     try:
         c = a / b
-    except:
-        return None
+    except ZeroDivisionError:
+        c = None
     finally:
         print("Inside Result: {}".format(c))
     return c

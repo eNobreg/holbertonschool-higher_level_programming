1f88bd5dd42751732cc540533710a2cafdae4a4d
eNobreg <ezra.nobrega@outlook.com>
2020-01-08 14:21:21 -0500

    [New] 101 advanced task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        r = fct(*args)
+        return r
+    except Exception as err: 
+        print("Exception: {}".format(err), file=stderr)
+        return None

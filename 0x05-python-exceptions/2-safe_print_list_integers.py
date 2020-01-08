54ffabdfe815841475bec73c227beb5c8b7ea53a
eNobreg <ezra.nobrega@outlook.com>
2020-01-08 12:30:59 -0500

    [Change] Made it match task 0 so the count is correct

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    count = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except TypeError:
-            continue
-        except ValueError:
+            count += 0;
+        except (TypeError, ValueError):
             continue
     print()
-    return i
+    return count

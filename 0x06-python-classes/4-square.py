5dec29b0b652b437a635510e58c95fc5110e4037
eNobreg <ezra.nobrega@outlook.com>
2020-01-09 15:47:48 -0500

    [Fix] Made pep8 complient

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """ File to define a square  """
+
+
 class Square:
     """ A square of size """
     def __init__(self, size=0):
         """ Initialized Size  """
         self.__size = size
+
     def area(self):
         """ Area of square """
         return(self.__size * self.__size)
+
     @property
     def size(self):
         return (self.__size)
+
     @size.setter
     def size(self, value):
         """ sets the size """
@@ -19,5 +24,4 @@ class Square:
         elif(value < 0):
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value 
-
+            self.__size = value
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """ File to define a square  """
+
+
 class Square:
     """ A square of size """
     def __init__(self, size=0):
         """ Initialized Size  """
         self.size = size
+
     def area(self):
         """ Area of square """
         return(self.__size * self.__size)
+
     @property
     def size(self):
         return (self.__size)
+
     @size.setter
     def size(self, value):
         """ sets the size """
@@ -19,7 +24,8 @@ class Square:
         elif(value < 0):
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value 
+            self.__size = value
+
     def my_print(self):
         """ Print a square """
         for i in range(0, self.size):

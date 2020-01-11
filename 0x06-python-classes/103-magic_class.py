e2cb35f6b106952eb672b75f0fa6a6f6ce20b94d
eNobreg <ezra.nobrega@outlook.com>
2020-01-10 23:38:07 -0500

    [New] Initial file push

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This doc """
+
+
+import math
+class MagicClass:
+    """ This class """
+
+    def __init__(self, radius=0):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return self.radius**2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius

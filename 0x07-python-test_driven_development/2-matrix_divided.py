887d07aba6e8202063bab9709c5d08d4176e1400
eNobreg <ezra.nobrega@outlook.com>
2020-01-13 16:03:45 -0500

    [New]

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,7 +6,7 @@ def matrix_divided(matrix, div):
     """ Divide a Matrix and check Errors """
 
     if div is 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero")  
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
@@ -14,6 +14,12 @@ def matrix_divided(matrix, div):
         raise TypeError("matrix must be a matrix (list of lists) "
                         "of integers/floats")
     for m_list in matrix:
+        if isinstance(m_list, list) is False:
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
+        if len(m_list) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
         if not all(isinstance(x, (int, float)) for x in m_list):
                 raise TypeError("matrix must be a matrix (list of lists) of "
                                 "integers/floats")
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This Module  """
+
+
+def text_indentation(text):
+    """ Indents text at special characters  """
+    rep_char = ['.', ':', '?']
+    new_str = ""
+    i = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.strip()
+    while i < len(text):
+        if text[i] in rep_char:
+            new_str += text[i]
+            i += 1
+            if i is not len(text):
+                if (text[i] is ' '):
+                    while text[i] == ' ':
+                        i += 1 
+            new_str += "\n\n"
+            continue;
+        new_str += text[i]
+        i += 1
+    print(new_str, end='')

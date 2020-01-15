0537ea59d369f748e060e9a7a2cbab01891cddeb
eNobreg <ezra.nobrega@outlook.com>
2020-01-14 19:46:26 -0500

    [Fix] checked error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,6 @@ class Rectangle:
 
     def perimeter(self):
         """ Perimeter function """
+        if self.width == 0 or self.height == 0:
+            return 0
         return (2*(self.width + self.height))

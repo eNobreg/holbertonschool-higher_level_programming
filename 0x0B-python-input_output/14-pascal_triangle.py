d94f1ba7782823b418fd2e2b47d7bf59cf25f63a
eNobreg <ezra.nobrega@outlook.com>
2020-01-21 18:32:02 -0500

    [Fix] Pep8

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Triangle Print """
 
+
 def pascal_triangle(n):
     """ Making a matrix """
 
@@ -8,13 +9,11 @@ def pascal_triangle(n):
     matrix = []
     if n <= 0:
         return []
-    for j in range(1, n + 1):  
-        num = 1;
-        for i in range(1, j + 1):  
-            new_list.append(num) 
-            num = int(num * (j - i) / i);
+    for j in range(1, n + 1):
+        num = 1
+        for i in range(1, j + 1):
+            new_list.append(num)
+            num = int(num * (j - i) / i)
         matrix.append(new_list)
         new_list = []
     return matrix
-
-

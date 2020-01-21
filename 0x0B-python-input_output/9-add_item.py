16c8996f193b8bbc265ce98436ff91ec4598d2fb
eNobreg <ezra.nobrega@outlook.com>
2020-01-21 17:07:52 -0500

    [Fix] Import guard

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import json
 from sys import argv
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+if __name__ == "__main__"
+    load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 count = 0

76bb6070f554ff78abb340b3bd3adc66ed6fdfe6
eNobreg <ezra.nobrega@outlook.com>
2020-01-21 17:35:00 -0500

    [Fix] Fixed pep8

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,21 +2,21 @@
 import json
 from os.path import exists
 from sys import argv
+
 if __name__ == "__main__":
-    load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+    from_json_file = __import__('8-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-filename = "add_item.json"
-count = 0
+    filename = "add_item.json"
 
-if (exists(filename)):
-    old_list = load_from_json_file(filename)
-else:
     old_list = []
 
-i = 1
-while i < (len(argv)):
-    old_list.append(argv[i])
-    i += 1
+    if (exists(filename)):
+        old_list = from_json_file(filename)
 
-save_to_json_file(old_list, filename)
+    i = 1
+    while i < (len(argv)):
+        old_list.append(argv[i])
+        i += 1
+
+    save_to_json_file(old_list, filename)

b9950acd2037b85faea6924585863ec077793ccd
eNobreg <ezra.nobrega@outlook.com>
2020-01-21 17:25:33 -0500

    test

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import json
 from sys import argv
-if __name__ == "__main__"
+if __name__ == "__main__":
     load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 

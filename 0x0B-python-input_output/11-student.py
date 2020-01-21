156630ddb114f689cea78ee7dbe4248e3621a444
eNobreg <ezra.nobrega@outlook.com>
2020-01-21 17:36:33 -0500

    Tests

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,6 +4,7 @@
 
 class Student():
     """ Student Class """
+
     def __init__(self, first_name, last_name, age):
         """ Instantiation for class """
         self.first_name = first_name
--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -4,6 +4,7 @@
 
 class Student():
     """ Student Class """
+
     def __init__(self, first_name, last_name, age):
         """ Instantiation for class """
         self.first_name = first_name
@@ -12,8 +13,7 @@ class Student():
 
     def to_json(self, attrs=None):
         """ json repr """
-        if attrs and all(isinstance(x, str) for x in attrs): 
+        if attrs and all(isinstance(x, str) for x in attrs):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         else:
             return self.__dict__
-

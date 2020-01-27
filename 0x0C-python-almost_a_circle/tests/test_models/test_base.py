d19468b5841550d3b4a1b4a9b17a432090895d0f
eNobreg <ezra.nobrega@outlook.com>
2020-01-27 15:55:29 -0500

    new

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,25 +4,52 @@
 import os
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 class TestMaxInteger(unittest.TestCase):
     """ Test Cases """
 
     def setUp(self):
         """ Setup """
-        Base.__nb_objects = 0
+        Base._Base__nb_objects = 0
+
+    def test_id_types(self):
+        """ Unit test """
+        a0 = Base("a")
+        self.assertEqual(a0.id, 'a')
 
     def test_empty_id(self):
+        """ Unit test """
         a0 = Base()
         self.assertEqual(a0.id, 1)
 
+    def test_nagative_id(self):
+        """ Unit Test """
+        a0 = Base(-1)
+        self.assertEqual(a0.id, -1)
+
     def test_id(self):
+        """ Unit test """
         a0 = Base(89)
         self.assertEqual(a0.id, 89)
     
     def test_many_empty(self):
+        """ Unit test """
         a0 = Base()
-        print(a0.id)
         a1 = Base()
         a2 = Base()
-        self.assertEqual(a2.id, 4)
+        self.assertEqual(a2.id, 3)
+
+    def test_none_id(self):
+        a0 = Base(None)
+        self.assertEqual(a0.id, 1)
+
+    def test_to_inf(self):
+        a0 = Base(float('inf'))
+        self.assertEqual(a0.id, float('inf'))
+
+    def test_json(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertIsInstance(json_dictionary, str)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,13 +7,41 @@ from models.base import Base
 from models.rectangle import Rectangle
 
 
-class TestMaxInteger(unittest.TestCase):
+class r_t(unittest.TestCase):
     """ Test Cases """
+    r1 = Rectangle(5, 5)
+    r2 = Rectangle(3, 3)
+    r3 = Rectangle(9, 9)
 
     def setUp(self):
         """ Setup """
-        Base.__nb_objects = 0
+        r3 = Rectangle(9, 9)
 
     def test_inheritance(self):
         self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_area(self):
+        """ tests """
+        r1 = Rectangle(5, 5)
+        r2 = Rectangle(3, 3)
+        r3 = Rectangle(9, 9)
+        self.assertEqual(r1.area(), 25)
+        self.assertEqual(r2.area(), 9)
+        self.assertEqual(r3.area(), 81)
+
+    def test_negatives(self):
+        """ Nagative """
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-5, -4)
+
+    def test_none(self):
+        """ Testing none input """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, None)
+    
+    def test_equal_dicts(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        diction = r1.to_dictionary()
+        self.assertEqual(r1.to_dictionary(), diction)
+
 

19fb4e4b6ba84f4e9ab2c390f6cd38043f06d269
eNobreg <ezra.nobrega@outlook.com>
2020-01-27 17:48:11 -0500

    testing new method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,40 +10,46 @@ class Square(Rectangle):
     """ Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ This and That """
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}".format(self.id,
-                                                  self.x, self.y, self.size))
+                                                  self.x, self.y, self.width))
 
     @property
     def size(self):
-        """ Getter for size """
+        """ Getter for size
         return self.__size
+        """
+        return self.width
 
     @size.setter
     def size(self, value):
-        """ Sizes """
+        """ Sizes
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__size = value
+        """
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """ Update the Square """
         if len(args) > 0:
             try:
                 self.id = args[0]
-                self.size = args[1]
+                self.width = args[1]
+                self.height = args[1]
                 self.x = args[2]
                 self.y = args[3]
             except:
                 pass
         else:
             self.id = kwargs.get('id', self.id)
-            self.size = kwargs.get('size', self.size)
+            self.width = kwargs.get('size', self.size)
+            self.height = kwargs.get('size', self.size)
             self.x = kwargs.get('x', self.x)
             self.y = kwargs.get('y', self.y)
 

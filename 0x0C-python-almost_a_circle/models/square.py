808e57e6ed323e0a2a81f0c6eb17dbf9847c87a9
eNobreg <ezra.nobrega@outlook.com>
2020-01-27 12:56:47 -0500

    Testing

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
     """ Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ This and That """
+        super().__init__(size, size, x, y, id)
         self.size = size
-        super().__init__(self.size, self.size, x, y, id)
 
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}".format(self.id,
@@ -20,16 +20,13 @@ class Square(Rectangle):
     @property
     def size(self):
         """ Getter for size """
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """ Sizes """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = vaule
 
     def update(self, *args, **kwargs):
         """ Update the Square """

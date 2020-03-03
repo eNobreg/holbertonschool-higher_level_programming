b3d853e7abe865843bb7717c31b8ee7f57df48b0
eNobreg <ezra.nobrega@outlook.com>
2020-03-03 15:32:12 -0500

    [New] Initial push

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Select city and states
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.idi ORDER BY cities.id ASC;

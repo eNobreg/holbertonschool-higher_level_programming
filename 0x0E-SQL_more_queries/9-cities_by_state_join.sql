aa7309f55336a2bda894e05421d05c654768bf3f
eNobreg <ezra.nobrega@outlook.com>
2020-03-03 15:33:29 -0500

    [Fix] Removed extra character

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Select city and states
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.idi ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;

8cc2aa5fe488e2afa736efefa06f16fec5f28b58
eNobreg <ezra.nobrega@outlook.com>
2020-03-28 12:40:46 -0400

    [Fix] Pep8 check

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 # Comment
 if __name__ == "__main__":
-	import MySQLdb
-	from sys import argv
+    import MySQLdb
+    from sys import argv
 
-	connection = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
-	cursor = connection.cursor()
-	execute = cursor.execute("SELECT id, name FROM states ORDER BY states.id;")
-	for i in range(0, execute):
-		entry = cursor.fetchone()
-		if entry[1][0] == 'N':
-			print(entry)
-	cursor.close()
-	connection.close()
+    connection = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = connection.cursor()
+    execute = cursor.execute("SELECT id, name FROM states ORDER BY states.id;")
+    for i in range(0, execute):
+        entry = cursor.fetchone()
+        if entry[1][0] == 'N':
+            print(entry)
+    cursor.close()
+    connection.close()

a3b381ddbfb5d6c5ea1b4128af805c9239e9fa93
eNobreg <ezra.nobrega@outlook.com>
2020-03-28 13:09:56 -0400

    [Update] Added LIKE to WHERE

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     connection = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cursor = connection.cursor()
     execute = cursor.execute("SELECT id, name FROM states\
-                            WHERE states.name='{}'\
+                            WHERE states.name LIKE '%{}%'\
                             ORDER BY states.id;".format(argv[4]))
     for i in range(0, execute):
         entry = cursor.fetchone()

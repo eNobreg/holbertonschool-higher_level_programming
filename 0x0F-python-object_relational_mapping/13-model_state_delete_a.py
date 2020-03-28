e1f97c4f96278f073d1d12f5b4dc1e024414b30a
eNobreg <ezra.nobrega@outlook.com>
2020-03-28 17:03:45 -0400

    [New] Initial push

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Comment
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    session.query(State).filter(State.name.contains('a')).delete()
+
+    session.commit()
+    session.close()

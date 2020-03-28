46f8b434371e3c342c984322310701109667bd0a
eNobreg <ezra.nobrega@outlook.com>
2020-03-28 13:01:09 -0400

    [New] Initial push

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Everything
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column('id', Integer(), nullable=False, primary_key=True,
+                autoincrement=True)
+    name = Column('name', String(128), nullable=False)

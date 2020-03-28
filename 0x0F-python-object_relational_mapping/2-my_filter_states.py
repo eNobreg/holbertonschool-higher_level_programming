3084fb22795aab738583576f73d3e5851b399d0c
eNobreg <ezra.nobrega@outlook.com>
2020-03-28 13:42:59 -0400

    [Fix] Changed to SELECT *

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
 
     connection = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cursor = connection.cursor()
-    execute = cursor.execute("SELECT id, name FROM states\
-                            WHERE states.name LIKE '%{}%'\
+    execute = cursor.execute("SELECT * FROM states\
+                            WHERE states.name='{}'\
                             ORDER BY states.id;".format(argv[4]))
     for i in range(0, execute):
         entry = cursor.fetchone()

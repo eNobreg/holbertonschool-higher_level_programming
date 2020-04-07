477ba620764f51d3a2716a60af1dbea6802ffec4
eNobreg <ezra.nobrega@outlook.com>
2020-04-07 16:05:30 -0400

    [New] Initial push

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Docstring
+"""
+
+
+def find_peak(ln):
+    """
+    Trojan
+    """
+    length = len(ln) - 1
+
+    if not ln:
+        return None
+
+    if ln[0] >= ln[1]:
+        return ln[0]
+
+    if ln[length] > ln[length - 1]:
+        return ln[length]
+
+    for i in range(1, length):
+        if ln[i] > ln[i + 1] and ln[i] > ln[i - 1]:
+            return(ln[i])
+    
+    return None

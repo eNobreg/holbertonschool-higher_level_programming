c3b44380d48f883b4a98a62c10758659ca7b9b66
eNobreg <ezra.nobrega@outlook.com>
2020-04-13 14:08:17 -0400

    [Fix] Import statements again

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,8 @@ Docstrig
 """
 
 from urllib import request
-from sys import argv
+import sys
 
 
-with request.urlopen(argv[1]) as response:
+with request.urlopen(sys.argv[1]) as response:
     print(response.info()['X-Request-Id'])

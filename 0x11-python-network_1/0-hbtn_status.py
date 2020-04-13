fa7c8a09a5e90368e2e56a458f86cbe62967f086
eNobreg <ezra.nobrega@outlook.com>
2020-04-13 13:33:00 -0400

    [New] Initial push

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Docstrig
+"""
+
+from urllib import request
+
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body Response:")
+    print("\t- type: {}".format(str(type(html))))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: '{}'".format(html.decode()))

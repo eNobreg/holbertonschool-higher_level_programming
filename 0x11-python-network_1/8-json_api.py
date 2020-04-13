91214b5f0154fbf9d56467648d9e2986c5b43d3e
eNobreg <ezra.nobrega@outlook.com>
2020-04-13 17:55:45 -0400

    [New] Initial push

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Docstrig
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    try:
+        json = r.json()
+        if not json:
+            print("No result")
+        else:
+            print('[{}] {}'.format(json['id'], json['name']))
+    except ValueError:
+            print("Not a valid JSON")

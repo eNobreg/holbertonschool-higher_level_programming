8112770efce8575916bedf45370f783b9a1fba3a
eNobreg <ezra.nobrega@outlook.com>
2020-04-13 13:55:34 -0400

    [Fix] Capital letter

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ from urllib import request
 
 with request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print("Body Response:")
+    print("Body response:")
     print("\t- type: {}".format(str(type(html))))
     print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf8')))

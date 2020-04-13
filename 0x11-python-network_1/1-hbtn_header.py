18237263710bca918c6350abf81c984e182ec795
eNobreg <ezra.nobrega@outlook.com>
2020-04-13 14:05:15 -0400

    [New] Initial push

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Docstrig
+"""
+
+from urllib import parse
+from urllib import request
+from sys import argv
+
+
+with request.urlopen(argv[1]) as response:
+    print(response.info()['X-Request-Id'])

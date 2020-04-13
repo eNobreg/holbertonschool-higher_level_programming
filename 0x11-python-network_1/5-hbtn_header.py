dd5c72c4e1e52ef84e6d059327cfecffd46ba529
eNobreg <ezra.nobrega@outlook.com>
2020-04-13 16:36:13 -0400

    [New] Initial push

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Docstrig
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])

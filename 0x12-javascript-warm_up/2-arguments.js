f906fee3298fc0a8aa6305fbe7e28ac04ca4adbe
eNobreg <ezra.nobrega@outlook.com>
2020-05-05 12:03:57 -0400

    [New] Initial push

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length === 1) {
+  console.log('Argument found');
+} else if (args.length > 1) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}

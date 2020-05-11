5905aae667d7a15b51270e4e25389e16618d55d3
eNobreg <ezra.nobrega@outlook.com>
2020-05-11 13:49:14 -0400

    [New] Initial push

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,4 +5,10 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
 };

341c71bbcf66e2191fd71290c293b460a94b961c
eNobreg <ezra.nobrega@outlook.com>
2020-05-12 13:00:43 -0400

    [Fix] semistandard fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,16 +4,14 @@ const args = process.argv.slice(2);
 
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + args[0],
-  method: 'GET',
-  headers: {
-    Accept: 'application/json'
-  }
+  method: 'GET'
 };
 
 request(options, function (err, res, body) {
-  if (err) {
+  if (body) {
     console.log(err);
+  } else {
+    const json = JSON.parse(body);
+    console.log(json.title);
   }
-  const json = JSON.parse(body);
-  console.log(json.title);
 });

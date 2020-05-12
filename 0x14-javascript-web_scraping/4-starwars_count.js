d1b00a59c7348e60bfbd189692d141ce52adc460
eNobreg <ezra.nobrega@outlook.com>
2020-05-12 18:14:58 -0400

    [New] Initial push

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,17 +5,18 @@ const args = process.argv.slice(2);
 
 const url = args[0];
 request(url, function (err, res, body) {
+  
   if (err) {
     console.log(err);
-  } else {
-    let total = 0;
-    const json = JSON.parse(body).results;
-    for (const entry of json) {
-      if (entry.characters.includes('18')) {
+  }
+  let total = 0;
+  const json = JSON.parse(body).results;
+  for (const entry of json) {
+    for (charUrl in entry.characters) {
+      if (charUrl.includes('18')) {
         total += 1;
       }
     }
-    
   }
   console.log(total);
 });

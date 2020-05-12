3c6bd27a58b856acd53a6c8a758eb75a67db6645
eNobreg <ezra.nobrega@outlook.com>
2020-05-12 18:06:57 -0400

    [Update]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,14 +7,15 @@ const url = args[0];
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
-  } else if (res.statusCode === 200) {
+  } else {
     let total = 0;
     const json = JSON.parse(body).results;
     for (const entry of json) {
-      if (entry.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      if (entry.characters.includes('18')) {
         total += 1;
       }
     }
-    console.log(total);
+    
   }
+  console.log(total);
 });
